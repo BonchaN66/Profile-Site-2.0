f952bb3e26a949e389c53d9fb6e37be7449c51f4
BonchaN66 <t_sk8@icloud.com>
2025-08-11 20:52:58 +0900

    React＋TypeScript＋Vite+Node.js環境構築（既存静的サイトアップデート）

---
+++ Profile-Site/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Section from './Sections'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+};
+
+export default App; //export const App = () => {}でリファクタ検討
